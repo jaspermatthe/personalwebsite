451acf1d16f255a22d882238a0a1b8c2aa08d297
Jasper Matthe <matthejasper@gmail.com>
2023-01-13 20:14:57 +0100

    message

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
 
   // Uncomment when pushing for deployment (npm run build and run generate)
-  // target: 'static',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
